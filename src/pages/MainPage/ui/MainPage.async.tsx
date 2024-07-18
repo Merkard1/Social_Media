fb6038384cc64214c9ccb986b6d74f69dfae2712
Vladyslav <merkard@Vladyslavs-MacBook-Pro-2.local>
2024-07-18 15:20:08 -0600

    check actions

--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,7 +1,11 @@
 import { lazy } from 'react';
 
-export const MainPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-    setTimeout(() => resolve(import('./MainPage')), 1500);
-}));
+const MainPageAsync = lazy(
+  () =>
+    new Promise((resolve) => {
+      // @ts-ignore
+      setTimeout(() => resolve(import('./MainPage')), 1500);
+    }),
+);
+
+export default MainPageAsync;
